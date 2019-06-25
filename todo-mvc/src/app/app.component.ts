1e874f8486d02d9bb67debce902b2f3e1505add4
daidv <daidv@2nf.com.vn>
2019-06-25 17:03:24 +0700

    aa

--- todo-mvc/src/app/app.component.ts
+++ todo-mvc/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-mvc';
+
+  constructor(
+    private todoService: TodoService
+  ){}
+
+  ngOnInit(){
+
+  }
 }
--- todo-mvc/src/app/services/todo.service.ts
+++ todo-mvc/src/app/services/todo.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Filter } from '../models/filtering.model';
+import { LocalStorageService } from './local-storage.service';
+// import { timingSafeEqual } from 'crypto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  private static readonly TodoStorageKey = 'todos';
+
+  private todos: Todo[];
+  private filteredTodos: Todo[];
+  private lengthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  private displayTodosSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<Todo[]>([]);
+  private currentFilter: Filter = Filter.All;
+
+  todos$: Observable<Todo[]> = this.displayTodosSubject.asObservable();
+  length$: Observable<number> = this.lengthSubject.asObservable();
+
+  constructor(private storageService: LocalStorageService) { }
+
+  fetchFromLocalStorage(): void {
+    this.todos = this.storageService.getValue<Todo[]>(TodoService.TodoStorageKey) || [];
+    this.filteredTodos = [...this.todos.map(todo => ({ ...todo }))];
+    this.updateTodosData();
+  }
+
+  updateToLocalStorage(): void {
+    this.storageService.setObject(TodoService.TodoStorageKey, this.todos);
+    this.filterTodos(this.currentFilter, false);
+    this.updateTodosData();
+  }
+
+  filterTodos(filter: Filter, isFiltering: boolean = true) {
+    this.currentFilter = filter;
+    switch (filter) {
+      case Filter.Active:
+        this.filteredTodos = this.todos.filter(todo => !todo.isCompleted);
+        break;
+      case Filter.Completed:
+        this.filteredTodos = this.todos.filter(todo => todo.isCompleted);
+        break;
+      case Filter.All:
+        this.filteredTodos = [...this.todos.map(todo => ({ ...todo }))];
+        break;
+    }
+    if (isFiltering) {
+      this.updateTodosData();
+    }
+  }
+
+  private updateTodosData() {
+    this.displayTodosSubject.next(this.filteredTodos);
+    this.lengthSubject.next(this.todos.length);
+  }
+
+
 }
