a372fb2b83a29bbf74d4acbb5649ff61543042e4
BienPham <phamdanhbien@gmail.com>
2019-06-29 18:19:31 +0700

    aaaa

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { WorkComponent } from './components/work/work.component';
+import { PersonComponent } from './components/person/person.component';
+import { ListPersonComponent } from './components/list-person/list-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkComponent,
+    PersonComponent,
+    ListPersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
